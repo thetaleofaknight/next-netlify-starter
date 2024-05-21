0cca1ff3731d6e61ef7d580bbddd82ecca9dae30
thetaleofaknight <the.tale.of.a.knight@gmail.com>
2024-05-21 23:35:53 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,9 @@ import Footer from '@components/Footer'
 export default function Home() {
   return (
     <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Join the Journey" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
